53e490cf7aa9c9cd260fda8e62daf4714e42d543
camilobreia <clbreia@gmail.com>
2023-04-18 15:34:48 -0300

    .

---
+++ horadosistema/HoraDoSistema.java
@@ -0,0 +1,9 @@
+package horadosistema;
+
+import java.util.Date;
+
+public class HoraDoSistema {
+    Date relogio = new Date();
+    System.out.println("A hora do sistema é: ");
+    System.out.println(relogio.toString());
+}
