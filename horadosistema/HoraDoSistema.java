784cdb612e3dc07902b687f14dac3c8ca7c08c76
camilobreia <clbreia@gmail.com>
2023-04-19 21:43:50 -0300

    .

--- horadosistema/HoraDoSistema.java
+++ horadosistema/HoraDoSistema.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class HoraDoSistema {
     public static void main(String[] args) {       
     
-        Date relogio = new Date();
+        Date relogio = new Date(); // new => cria um objeto da classe Date
         System.out.println("A hora do sistema é: ");
         System.out.println(relogio.toString());
     }
