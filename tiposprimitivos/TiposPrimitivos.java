1dad2252cc62c24412a037bbfd0f475117bb546e
camilobreia <clbreia@gmail.com>
2023-05-16 11:24:45 -0300

    Update TiposPrimitivos.java

--- tiposprimitivos/TiposPrimitivos.java
+++ tiposprimitivos/TiposPrimitivos.java
@@ -1,13 +1,19 @@
 package tiposprimitivos;
 
+import java.util.Scanner;
+
 public class TiposPrimitivos {
     public static void main(String[] args) {
     
-    String nome = "Ana";
-    float nota = 8.5f;
+    Scanner teclado = new Scanner(System.in);
+    String nome = teclado.nextLine(); //para receber a informação de entrada
+    float nota = teclado.nextFloat(); 
+    teclado.close();
     // System.out.println("A nota é " + nota);
 
-    System.out.printf("A nota de %s é %.2f", nome, nota); //SOUT formatado (o número equivale a quantidade de casa decimais)
+    // System.out.printf("A nota de %s é %.2f", nome, nota); //SOUT formatado (o número equivale a quantidade de casa decimais)
+
+    System.out.format("A nota de %s é de %.3f", nome, nota); //mesma coisa que o printf
 
     }
 }
