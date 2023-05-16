31be35b884d8217a886be63cc99adaa94a3a5b24
camilobreia <clbreia@gmail.com>
2023-05-16 11:13:22 -0300

    .

--- tiposprimitivos/TiposPrimitivos.java
+++ tiposprimitivos/TiposPrimitivos.java
@@ -1,6 +1,13 @@
 package tiposprimitivos;
 
 public class TiposPrimitivos {
+    public static void main(String[] args) {
+    
+    String nome = "Ana";
     float nota = 8.5f;
-    System.out.print("A nota é " + nota);
+    // System.out.println("A nota é " + nota);
+
+    System.out.printf("A nota de %s é %.2f", nome, nota); //SOUT formatado (o número equivale a quantidade de casa decimais)
+
+    }
 }
