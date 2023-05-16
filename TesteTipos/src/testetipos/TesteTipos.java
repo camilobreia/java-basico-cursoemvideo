79e2d0341cf0951f4958a3423ae7cab52746fbf9
camilobreia <clbreia@gmail.com>
2023-05-16 14:54:08 -0300

    .

---
+++ TesteTipos/src/testetipos/TesteTipos.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testetipos;
+
+/**
+ *
+ * @author clb
+ */
+public class TesteTipos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       /* int idade = 30;
+                 String valor = Integer.toString(idade); //usamos o método toString para converter o inteiro em 
+                 System.out.println(valor);*/
+       
+       /* String valor = "30";
+                 int idade = Integer.parseInt(valor);
+                 System.out.println(idade);*/ // para converter string em inteiro
+
+        String valor = "30.5";
+        float idade = Float.parseFloat(valor);
+        System.out.println(idade);
+    }
+    
+}
